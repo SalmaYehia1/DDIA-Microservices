edadf4de29bcc86048565324d4d61ab77f844f8e
Salma Yehia <es-salma.yehia2027@alexu.edu.eg>
2026-04-13 13:19:44 +0200

    grpc microservice

---
+++ trending-service/src/main/java/com/example/trending/TrendingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.trending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class TrendingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrendingServiceApplication.class, args);
+    }
+}
